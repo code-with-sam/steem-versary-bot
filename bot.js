3e757986ec98d9e74e281e1469a60b3779d48d52
Sam Billingham <sambillie.sb@gmail.com>
2018-01-03 09:55:35 +0000

    test add vote + comment

--- bot.js
+++ bot.js
@@ -6,17 +6,19 @@ const rp = require('request-promise-native');
 const steem = require('steem');
 const prettyjson = require('prettyjson');
 
+const BOT_ACCOUNT_NAME = '';
+const BOT_ACCOUNT_WIF = ''
 
-// get list of relevant versary users from steemSQL
-let dateToday = new Date();
-let dateLastYear = new Date(dateToday.getTime() - (365 * 24 * 60 * 60 * 1000));
-let dateLastYearTomorrow = new Date(dateToday.getTime() + (24 * 60 * 60 * 1000) - (365 * 24 * 60 * 60 * 1000));
-let lastYeaToday = dateLastYear.getFullYear()+'/'+(dateLastYear.getMonth()+1)+'/'+dateLastYear.getDate();
-let lastYearTomorrow = dateLastYearTomorrow.getFullYear()+'/'+(dateLastYearTomorrow.getMonth()+1)+'/'+dateLastYearTomorrow.getDate() ;
-
-let sqlQuery = `SELECT * FROM TxAccountCreates WHERE timestamp >= '${lastYeaToday}' AND timestamp < '${lastYearTomorrow}' ORDER BY CONVERT(DATE, timestamp) DESC`
 
 function getAniversaryData(){
+    // get list of relevant versary users from steemSQL
+    let dateToday = new Date();
+    let dateLastYear = new Date(dateToday.getTime() - (365 * 24 * 60 * 60 * 1000));
+    let dateLastYearTomorrow = new Date(dateToday.getTime() + (24 * 60 * 60 * 1000) - (365 * 24 * 60 * 60 * 1000));
+    let lastYeaToday = dateLastYear.getFullYear()+'/'+(dateLastYear.getMonth()+1)+'/'+dateLastYear.getDate();
+    let lastYearTomorrow = dateLastYearTomorrow.getFullYear()+'/'+(dateLastYearTomorrow.getMonth()+1)+'/'+dateLastYearTomorrow.getDate() ;
+
+    let sqlQuery = `SELECT * FROM TxAccountCreates WHERE timestamp >= '${lastYeaToday}' AND timestamp < '${lastYearTomorrow}' ORDER BY CONVERT(DATE, timestamp) DESC`
     return rp.post( {
       url: 'https://sql.steemhelpers.com/api',
       form : { query : sqlQuery },
@@ -61,7 +63,7 @@ function getLatestPost(users){
   })
 }
 
-// get latest post of each user who has post in the last 6 days
+get latest post of each user who has post in the last 6 days
 getAniversaryData()
   .then(data => processNamesToAccounts(data))
   .then(data => processActiveUsers(data))
@@ -75,16 +77,65 @@ getAniversaryData()
 
     console.log(activeNames);
     getLatestPost(activeNames).then(data => {
+
+
           data.forEach( (post) => {
-            console.log(post[0].url)
-            console.log(post[0].author)
-
-
+            // console.log(post[0].url.substring(1))
+            // console.log(post[0].author)
           })
         })
 
   })
+console.log(BOT_ACCOUNT_NAME)
+// console.log(BOT_ACCOUNT_WIF)
 
-// calculate vote share per user
+
+  function randomString() {
+    return '_' + Math.random().toString(36).substr(2, 16);
+  }
+  //
+  // let commentTitle = 'Your Steem-Versay has arrived, Congratulations';
+  // let uniqueString = randomString();
+  // let beneficiaries = [];
+  // beneficiaries.push({
+  //       account: 'sambillingham',
+  //       weight: 100*25
+  //   });
+  // let operations = [
+  //         ['comment',
+  //             {
+  //                 parent_author: 'sambillingham',
+  //                 parent_permlink: '20171226t151234088z-post',
+  //                 author: BOT_ACCOUNT_NAME,
+  //                 permlink: 'thisisjustatestyoucantotallyignoreitokaythanksplz',
+  //                 title: commentTitle,
+  //                 body: 'This Is Just A Test. Hey!',
+  //                 json_metadata : JSON.stringify({
+  //                 tags: 'steem-versary',
+  //                 app: 'steem-versary'
+  //                 })
+  //             }
+  //         ],
+  //         ['comment_options', {
+  //             author: BOT_ACCOUNT_NAME,
+  //             permlink: 'thisisjustatestyoucantotallyignoreitokaythanksplz',
+  //             max_accepted_payout: '100000.000 SBD',
+  //             percent_steem_dollars: 10000,
+  //             allow_votes: true,
+  //             allow_curation_rewards: true,
+  //             extensions: [
+  //                 [0, {
+  //                     beneficiaries: beneficiaries
+  //                 }]
+  //             ]
+  //         }]
+  //     ];
+  //     steem.broadcast.send(
+  //         { operations: operations, extensions: [] },
+  //         { posting: BOT_ACCOUNT_WIF },
+  //         function(err, result) {
+  //   console.log(err, result);
+  // });
 
 // vote with bot account
+// add comment with bot account
